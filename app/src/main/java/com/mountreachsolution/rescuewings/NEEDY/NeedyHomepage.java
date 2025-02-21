d4190fbafc3eee9d2b522d893ed28ab38c2307f9
Parth200611 <parthdahapute7@gmail.com>
2025-02-21 06:31:08 +0530

    commit

--- app/src/main/java/com/mountreachsolution/rescuewings/NEEDY/NeedyHomepage.java
+++ app/src/main/java/com/mountreachsolution/rescuewings/NEEDY/NeedyHomepage.java
@@ -1,22 +1,66 @@
 package com.mountreachsolution.rescuewings.NEEDY;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowInsetsController;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.mountreachsolution.rescuewings.R;
+import com.mountreachsolution.rescuewings.SAVIOUR.SaviourHomepage;
 
-public class NeedyHomepage extends AppCompatActivity {
+public class NeedyHomepage extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_needy_homepage);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.blue)));
+        Window window = getWindow();
+        window.setStatusBarColor(getResources().getColor(R.color.blue));
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false);
+            window.getInsetsController().setSystemBarsAppearance(0, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+        }
+
+        getWindow().setNavigationBarColor(ContextCompat.getColor(NeedyHomepage.this,R.color.white));
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        bottomNavigationView = findViewById(R.id.bottomnevigatiomuserhome);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        bottomNavigationView.setSelectedItemId(R.id.nvHome);
+
+    }
+   NeedyHome needyHome = new NeedyHome();
+    Addrequest addrequest = new Addrequest();
+    NeedyProfil needyProfil = new NeedyProfil();
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.nvHome){
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayoutuserhome,needyHome).commit();
+        }else if(item.getItemId()==R.id.nvAddRequest){
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayoutuserhome,addrequest).commit();
+        } else if(item.getItemId()==R.id.nvProfil){
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayoutuserhome,needyProfil).commit();
+
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/mountreachsolution/rescuewings/NEEDY/NeedyProfil.java
@@ -0,0 +1,236 @@
+package com.mountreachsolution.rescuewings.NEEDY;
+
+import static android.app.Activity.RESULT_OK;
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.mountreachsolution.rescuewings.LoginActivity;
+import com.mountreachsolution.rescuewings.R;
+import com.mountreachsolution.rescuewings.VolleyMultipartRequest;
+import com.mountreachsolution.rescuewings.urls;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import cz.msebera.android.httpclient.Header;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class NeedyProfil extends Fragment {
+    CircleImageView profileImage;
+    ImageButton editPhotoButton;
+    TextView tvUsername, tvWelcome, tvName, tvMobileNo, tvEmail, tvGender, tvUserType;
+    Button btnLogout;
+    String username;
+    Bitmap bitmap;
+    Uri filepath;
+    private  int pick_image_request=789;
+
+    private static final int PICK_IMAGE_REQUEST_PASS = 1;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_needy_profil, container, false);
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        username = sharedPreferences.getString("username", "Guest");
+        profileImage = view.findViewById(R.id.profileImage);
+        editPhotoButton = view.findViewById(R.id.editPhotoButton);
+        tvUsername = view.findViewById(R.id.tvUsername);
+        tvWelcome = view.findViewById(R.id.tvWelcome);
+        tvName = view.findViewById(R.id.tvname);
+        tvMobileNo = view.findViewById(R.id.tvMobileno);
+        tvEmail = view.findViewById(R.id.tvEmail);
+        tvGender = view.findViewById(R.id.tvGender);
+        tvUserType = view.findViewById(R.id.tvUserType);
+        btnLogout = view.findViewById(R.id.btnlogout);
+
+        editPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SelectUserProfileimage();
+
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logoutUser();
+            }
+        });
+        getData();
+        return view;
+    }
+    private void SelectUserProfileimage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Image For Profil"),pick_image_request);
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==pick_image_request && resultCode==RESULT_OK && data!=null){
+            filepath=data.getData();
+            try {
+                bitmap= MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),filepath);
+                profileImage.setImageBitmap(bitmap);
+                UserImageSaveTodatabase(bitmap,username);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    private void UserImageSaveTodatabase(Bitmap bitmap, String strTitle) {
+        VolleyMultipartRequest volleyMultipartRequest =  new VolleyMultipartRequest(Request.Method.POST, urls.profilimage, new Response.Listener<NetworkResponse>() {
+            @Override
+            public void onResponse(NetworkResponse response) {
+                Toast.makeText(getActivity(), "Image Save as Profil "+strTitle, Toast.LENGTH_SHORT).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), "Error: " + error.getMessage(), Toast.LENGTH_LONG).show();
+                String errorMsg = error.getMessage();
+                if (error.networkResponse != null && error.networkResponse.data != null) {
+                    errorMsg = new String(error.networkResponse.data);
+                }
+                Log.e("UploadError", errorMsg);
+                Toast.makeText(getActivity(), "Upload Error: " + errorMsg, Toast.LENGTH_LONG).show();
+
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parms = new HashMap<>();
+                parms.put("tags", strTitle); // Adjusted to match PHP parameter name
+                return parms;
+            }
+
+            @Override
+            protected Map<String, DataPart> getByteData() throws AuthFailureError {
+                Map<String,VolleyMultipartRequest.DataPart> parms = new HashMap<>();
+                long imagename = System.currentTimeMillis();
+                parms.put("pic",new VolleyMultipartRequest.DataPart(imagename+".jpeg",getfiledatafromBitmap(bitmap)));
+
+                return parms;
+
+            }
+
+        };
+        Volley.newRequestQueue(getActivity()).add(volleyMultipartRequest);
+    }
+
+    private byte[] getfiledatafromBitmap(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream  = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+
+
+
+
+
+    private void getData() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+
+        params.put("username",username);
+        client.post(urls.Profildata,params,new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                try {
+                    JSONArray jsonArray = response.getJSONArray("getProfildata");
+                    for (int i=0;i<jsonArray.length();i++){
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String name=jsonObject.getString("name");
+                        String mobileno=jsonObject.getString("mobile");
+                        String gender=jsonObject.getString("gender");
+                        String email=jsonObject.getString("email");
+                        String image=jsonObject.getString("image");
+                        String usertype=jsonObject.getString("usertype");
+                        tvName.setText(name);
+                        tvMobileNo.setText(mobileno);
+                        tvGender.setText(gender);
+                        tvEmail.setText(email);
+                        tvUserType.setText(usertype);
+                        tvUsername.setText(username);
+                        Glide.with(getActivity())
+                                .load(urls.address + "images/"+image)
+                                .skipMemoryCache(true)
+                                .error(R.drawable.baseline_person_24)// Resize the image to 800x800 pixels
+                                .into(profileImage);
+                    }
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+    }
+
+    private void logoutUser() {
+        // Clear saved login details from SharedPreferences
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear(); // Clear all saved data
+        editor.apply();
+
+        Toast.makeText(getActivity(), "Logged out successfully!", Toast.LENGTH_SHORT).show();
+
+        // Redirect to LoginActivity
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear backstack
+        startActivity(intent);
+        getActivity().finish();
+    }
+}
